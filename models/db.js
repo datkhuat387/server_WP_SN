1206386f858df495b5a293669593ba4a6e01fb63
datkhuat387 <datkhuat387@gmail.com>
2024-04-16 02:18:23 +0700

    update user api

--- models/db.js
+++ models/db.js
@@ -1,9 +1,10 @@
 var mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/wp_sn')
+mongoose.connect('mongodb+srv://datkhuat387:datkhuat387@cluster0.zyk7rmp.mongodb.net/wp_sn?retryWrites=true&w=majority&appName=Cluster0')
         .catch( (err)=>{
                 console.log("Loi ket noi CSDL: ");
                 console.log(err);
         });
         // mongodb://127.0.0.1:27017/
+        // mongodb://127.0.0.1:27017/wp_sn
 module.exports = { mongoose };
